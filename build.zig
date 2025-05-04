cee5e2d8e629d6072f32f29da386971c619ec961
Michael Lynch <git@mtlynch.io>
2025-05-04 07:58:25 -0400

    Update for Zig 0.14.0

--- build.zig
+++ build.zig
@@ -17,10 +17,11 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zig-c-simple",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe.addIncludePath(b.path("c-src"));
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -53,10 +54,11 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    unit_tests.addIncludePath(b.path("c-src"));
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
